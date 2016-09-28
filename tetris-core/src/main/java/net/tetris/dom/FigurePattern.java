ccaedea9b6685eff578e926aa462ec83480c4357
Diana Anisutina <diana.anisutina@gmail.com>
2016-09-29 00:15:46 +0700

    added zero (edited)
    removed field

--- tetris-core/src/main/java/net/tetris/dom/FigurePattern.java
+++ tetris-core/src/main/java/net/tetris/dom/FigurePattern.java
@@ -3,7 +3,7 @@ package net.tetris.dom;
 public class FigurePattern {
     private String[] rows;
     private int centerX;
-    public int centerY;
+    private int zero;
 
     public FigurePattern(int centerX, int centerY, String... rows) {
         this.rows = rows;
